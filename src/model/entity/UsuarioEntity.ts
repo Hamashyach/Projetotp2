e391a8022e2f32edc2bf7440b31adf70eb71242d
Steh_Hamashyach <137620415+Hamashyach@users.noreply.github.com>
2024-08-23 11:04:46 -0300

    Update UsuarioEntity.ts
    Correção de erros

--- src/model/entity/UsuarioEntity.ts
+++ src/model/entity/UsuarioEntity.ts
@@ -13,7 +13,7 @@ export class UsuarioEntity {
     private validatesInformation(idPessoa: any, senha: any) {
         let error = '';
         if (typeof idPessoa !== 'number' || typeof senha !== 'string') {
-            error += "Informações incompletas ou incorretas";
+            error += ("Informações incompletas ou incorretas");
         }
 
         if (error !== '') {
